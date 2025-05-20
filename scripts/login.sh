3441486d96522d2d0cadadfdccbc4ec009cca58a
Yashi Sharma <shyashi04@gmail.com>
2025-05-20 11:02:10 +0530

    Update login.sh

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-# Simple login interface (without password validation)
 while true; do
     echo -e "\nContainerized Virtual OS Login"
     read -p "Username: " username
     read -sp "Password: " password
     echo
 
-    # Just check if user exists (since Alpine may not support su password auth)
     if id "$username" &>/dev/null; then
-        echo -e "\nWelcome to Containerized Virtual OS, $username!"
-        su - "$username"
-        break
+        # Debug: Show stored hash (SECURITY RISK - DEBUG ONLY)
+        stored_hash=$(sudo grep "^$username:" /etc/shadow | cut -d: -f2)
+        
+        # Generate input hash
+        salt=$(echo "$stored_hash" | cut -d'$' -f3)
+        input_hash=$(openssl passwd -6 -salt "$salt" "$password")
+        
+        if [ "$stored_hash" = "$input_hash" ]; then
+            echo -e "\nWelcome to Containerized Virtual OS, $username!"
+            su - "$username"
+            break
+        else
+            echo "Invalid password. Please try again."
+        fi
     else
         echo "Invalid username. Please try again."
     fi
 done
+
